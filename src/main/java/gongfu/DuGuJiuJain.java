006391aeaac66f039ff793c2d60a5445de1f5bb2
dataumgit <dataum1@sina.cn>
2024-03-24 21:26:57 +0800

    独孤九剑

---
+++ src/main/java/gongfu/DuGuJiuJain.java
@@ -0,0 +1,14 @@
+package gongfu;
+
+public class DuGuJiuJain {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+    }
+
+}
+
