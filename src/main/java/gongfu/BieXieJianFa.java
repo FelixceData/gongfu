aab7d484e71bfe625c38b1998cffa93f609262c9
FelixceData <felixce@foxmail.com>
2024-03-24 21:11:11 +0800

    辟邪剑法

---
+++ src/main/java/gongfu/BieXieJianFa.java
@@ -0,0 +1,12 @@
+package gongfu;
+
+public class BieXieJianFa {
+    public static void main(String[] args) {
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+    }
+}
